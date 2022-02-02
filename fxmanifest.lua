0a027596640fa0ddb6e058b65f6214d948835abb
/Dilza Core <98855169+DilzaCompany@users.noreply.github.com>
2022-02-02 19:17:26 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'Dilza Company' -- Discord: https://discord.io/dilzalab
+description 'By https://github.com/DilzaCompany'
+version '1.0'
+
+ui_page 'html/ui.html'
+
+client_scripts {
+	'client.lua',
+}
+
+files {
+	'html/*.*'
+}
+
+export 'Alert'
